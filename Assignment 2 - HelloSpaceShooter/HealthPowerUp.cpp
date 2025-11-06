3aceed1f7be126ac1e0f9f26cc49a791b549d587
Greivinnn <wenwugreivin5@gmail.com>
2025-11-05 22:26:54 -0800

    working on assignment 2

---
+++ Assignment 2 - HelloSpaceShooter/HealthPowerUp.cpp
@@ -0,0 +1,97 @@
+#include "HealthPowerUp.h"
+#include "AnimSpriteSheet.h"
+#include "Ship.h"
+
+HealthPowerUp::HealthPowerUp()
+	: Entity()
+	, Collidable(15.0f)
+	, mImageId(0)
+	, mPosition(0.0f, 0.0f)
+	, mRotation(0.0f)
+	, mHealthBoost(0.0f)
+	, mActive(false)
+{
+}
+
+HealthPowerUp::~HealthPowerUp()
+{
+}
+
+void HealthPowerUp::Load()
+{
+	mImageId = X::LoadTexture("mushroom.png");
+	XASSERT(mImageId != 0, "HealthPowerUp: image did not load");
+	SetCollisionFilter(ET_SHIP);
+	mHealthBoost = 20.0f; 
+}
+
+void HealthPowerUp::Update(float deltaTime)
+{
+	if(!mActive)
+	{
+		return;
+	}
+
+	const float fallSpeed = 50.0f;
+	mPosition.y += fallSpeed * deltaTime;
+
+	const float screenH = (float)X::GetScreenHeight();
+	if (mPosition.y > screenH + 50.0f)
+	{
+		SetActive(false);
+	}
+}
+
+void HealthPowerUp::Render()
+{
+	if(!IsActive())
+	{
+		return;
+	}
+	X::DrawSprite(mImageId, mPosition, mRotation);
+	X::DrawScreenCircle(mPosition, GetRadius(), X::Colors::Lime);
+}
+
+void HealthPowerUp::Unload()
+{
+	
+}
+
+int HealthPowerUp::GetType() const
+{
+	return ET_POWERUP_HEALTH;
+}
+
+const X::Math::Vector2& HealthPowerUp::GetPosition() const
+{
+	return mPosition;
+}
+
+//void HealthPowerUp::OnCollision(Collidable* collidable)
+//{
+//	if (IsActive() && collidable->GetType() == ET_SHIP)
+//	{
+//		Ship* ship = 
+//			// do this
+//	}
+//}
+
+void HealthPowerUp::SetHealthBoost(float healthBoost)
+{
+	mHealthBoost = healthBoost;
+}
+
+float HealthPowerUp::GetHealthBoost() const
+{
+	return mHealthBoost;
+}
+
+void HealthPowerUp::SetActive(bool active)
+{
+	mActive = active;
+}
+
+bool HealthPowerUp::IsActive() const
+{
+	return mActive;
+}
---
+++ Assignment 2 - HelloSpaceShooter/HealthPowerUp.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Entity.h"
+#include "Collidable.h"
+
+class AnimSpriteSheet;	
+class Ship;
+
+class HealthPowerUp : public Entity, public Collidable
+{
+public:
+	HealthPowerUp();
+	~HealthPowerUp() override;
+	void Load() override;
+	void Update(float deltaTime) override;
+	void Render() override;
+	void Unload() override;
+
+	int GetType() const override;
+	const X::Math::Vector2& GetPosition() const override;
+	void OnCollision(Collidable* collidable) override;
+
+	void SetHealthBoost(float healthBoost);
+	float GetHealthBoost() const;
+	void SetActive(bool active);
+	bool IsActive() const;
+
+private:
+	X::TextureId mImageId;
+	X::Math::Vector2 mPosition;
+	float mRotation;
+	float mHealthBoost;
+	bool mActive;
+};
